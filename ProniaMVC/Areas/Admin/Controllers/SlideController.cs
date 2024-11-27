b087f83b90538a9a20967f440dadc10840f4a633
nuranavaliyeva <nuranavvaliyeva@gmail.com>
2024-11-27 12:37:05 +0400

    modified

--- ProniaMVC/Areas/Admin/Controllers/SlideController.cs
+++ ProniaMVC/Areas/Admin/Controllers/SlideController.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using ProniaMVC.DAL;
 using ProniaMVC.Models;
+using ProniaMVC.Utilities.Enums;
+using ProniaMVC.Utilities.Extensions;
 
 namespace ProniaMVC.Areas.Admin.Controllers
 {
@@ -9,16 +11,65 @@ namespace ProniaMVC.Areas.Admin.Controllers
     public class SlideController : Controller
     {
         private readonly AppDbContex _context;
+        private readonly IWebHostEnvironment _env;
 
-        public SlideController(AppDbContex context)
+        public SlideController(AppDbContex context, IWebHostEnvironment env)
         {
+          
             _context = context;
+            _env = env;
         }
+
+      
 
         public async  Task<IActionResult> Index()
         {
             List<Slide> slides = await _context.Slides.ToListAsync();
             return View(slides);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Slide slide)
+        {
+            if (!slide.Photo.ValidateType("image/"))
+            {
+                ModelState.AddModelError("Photo", "File type is incorrect");
+                return View(); 
+            }
+            if(slide.Photo.ValidateSize(Utilities.Enums.FileSize.MB, 2 ))
+            {
+                ModelState.AddModelError("Photo", "File Size must be less than 2 MB");
+                return View();
+            }
+
+            
+            slide.Image = await slide.Photo.CreateFileAsync(_env.WebRootPath, "assets","images","website-images");
+
+            //if (!ModelState.IsValid) return View();
+            await _context.Slides.AddAsync(slide);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if(id == null || id<1) return BadRequest();
+            
+                Slide slide = await _context.Slides.FirstOrDefaultAsync(s=> s.Id == id);
+
+            
+            if(slide is null) return NotFound();
+
+            slide.Image.DeleteFile(_env.WebRootPath, "assets","images","website-images");
+            _context.Slides.Remove(slide);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
---
+++ ProniaMVC/Utilities/Extensions/FileValidator.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Identity.Client;
+using ProniaMVC.Models;
+using ProniaMVC.Utilities.Enums;
+
+namespace ProniaMVC.Utilities.Extensions
+{
+    public static class FileValidator
+    {
+        
+        public static bool ValidateType( this IFormFile file, string type)
+        {
+            if(file.ContentType.Contains(type))
+            {
+                return true;
+            }
+            return false;
+        }
+        public static bool ValidateSize(this IFormFile file, FileSize fileSize, int size )
+        {
+            switch( fileSize )
+            {
+                case FileSize.KB:
+                    return file.Length <= size * 1024;
+                    case FileSize.MB:
+                    return file.Length <= size * 1024 * 1024;
+                    case FileSize.GB:
+                    return file.Length <= size * 1024 * 1024 * 1024;
+            }
+            return false;
+
+            
+
+        }
+        public static async Task<string> CreateFileAsync(this IFormFile file, params string[] roots)
+        {
+            string fileExtension = file.FileName.Substring(file.FileName.LastIndexOf('.'));
+            string fileName = string.Concat(Guid.NewGuid().ToString(), fileExtension);
+            string path = string.Empty;
+
+            for(int i=0; i<roots.Length; i++)
+            {
+                path=Path.Combine(path, roots[i]);
+            }
+            path = Path.Combine(path, fileName);
+
+           using (FileStream fileStream = new(path, FileMode.Create))
+            {
+                await file.CopyToAsync(fileStream);
+            }
+           return fileName;
+        }
+        public static void DeleteFile(this string fileName, params string[] roots)
+        {
+
+            string path = string.Empty;
+
+            for (int i = 0; i < roots.Length; i++)
+            {
+                path = Path.Combine(path, roots[i]);
+            }
+            path = Path.Combine(path, fileName);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+        }
+    }
+}
